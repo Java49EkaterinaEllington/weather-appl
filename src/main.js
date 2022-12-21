af52caeabc2b60409ec2a69f35ef9e76344d7f7d
catherineair <113204242+catherineair@users.noreply.github.com>
2022-12-21 16:06:02 +0200

    weather-app 26#

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { DataForm } from "./ui/DataForm.js";
+import { TemperaturesList } from "./ui/TemperaturesList.js";
+import { WeatherDataProcessor } from "./data/WeatherDataProcessor.js";
+
+const weatherProcessor = new WeatherDataProcessor();
+const params = 
+{
+    idForm: "form-request", idCity: "city", idDateFrom: "date_from",
+    idDateTo: "date_to", idHourFrom: "hour_from", idHourTo: "hour_to", idErrorMessage: 'error_message'
+};
+const dataForm = new DataForm(params);
+const resultsList = new TemperaturesList("weather-list");
+
+dataForm.addSubmitHandler((dataFromForm) => 
+{
+    weatherProcessor.getData(dataFromForm, (data) => 
+    {
+        resultsList.showResults(data);
+    });
+})
